095fa9907eb49a3c1cee039600b025bcf571ea2d
Nikita <jainnikita381@gmail.com>
2023-07-11 14:12:51 +0530

    Getting started with Blueprints

--- src/auth.py
+++ src/auth.py
@@ -1,3 +1,7 @@
 from flask import Blueprint
 
-auth = Blueprint("auth",__name__,url_prefix="api/v1/auth")
+auth = Blueprint("auth",__name__,url_prefix="/api/v1/auth")
+
+@auth.get('/me')
+def me():
+    return {"user":"me"}
--- src/bookmarks.py
+++ src/bookmarks.py
@@ -1,3 +1,3 @@
 from flask import Blueprint
 
-bookmarks = Blueprint("bookmarks",__name__,url_prefix="api/v1/bookmarks")
+bookmarks = Blueprint("bookmarks",__name__,url_prefix="/api/v1/bookmarks")
