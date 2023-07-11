bc34396eff9265defd04b2d892a38ed7c9a1b34d
Nikita <jainnikita381@gmail.com>
2023-07-11 14:02:59 +0530

    Getting started with Blueprints

--- src/__init__.py
+++ src/__init__.py
@@ -1,6 +1,8 @@
 import os
 
 from flask import Flask
+from src.auth import auth
+from src.bookmarks import bookmarks
 
 
 def create_app(test_config=None):
@@ -10,5 +12,8 @@ def create_app(test_config=None):
         app.config.from_mapping(SECRET_KEY=os.environ.get("SECRET_KEY"))
     else:
         app.config.from_mapping(test_config)
+
+    app.register_blueprint(auth)
+    app.register_blueprint(bookmarks)
 
     return app
---
+++ src/bookmarks.py
@@ -0,0 +1,3 @@
+from flask import Blueprint
+
+bookmarks = Blueprint("bookmarks",__name__,url_prefix="api/v1/bookmarks")
