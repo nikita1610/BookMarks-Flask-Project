04f95079ef63d2048706e9b1cfe59fbe739d2301
Nikita <jainnikita381@gmail.com>
2023-07-12 14:13:01 +0530

    Getting started with database

--- src/database.py
+++ src/database.py
@@ -1,3 +1,14 @@
+from datetime import datetime
+
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
+
+
+class User(db.Model):
+    id = db.column(db.Integer, primary_key=True)
+    username = db.column(db.String(80), unique=True, nullable=False)
+    email = db.column(db.String(120), unique=True, nullable=False)
+    password = db.column(db.Text(), nullable=False)
+    created_at = db.column(db.DateTime, default=datetime.now())
+    updated_at = db.column(db.DateTime, onupdate=datetime.now())
