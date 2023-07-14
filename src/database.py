236a8325e40437a6a0f5ff072990d4cdc3746c76
Nikita <jainnikita381@gmail.com>
2023-07-14 16:46:57 +0530

    Getting started with models

--- src/database.py
+++ src/database.py
@@ -9,6 +9,20 @@ class User(db.Model):
     id = db.column(db.Integer, primary_key=True)
     username = db.column(db.String(80), unique=True, nullable=False)
     email = db.column(db.String(120), unique=True, nullable=False)
-    password = db.column(db.Text(), nullable=False)
+    password = db.column(db.Text, nullable=False)
+    created_at = db.column(db.DateTime, default=datetime.now())
+    updated_at = db.column(db.DateTime, onupdate=datetime.now())
+
+
+    def __repr__(self):
+        return self.username
+    
+
+class Bookmark(db.Model):
+    id = db.column(db.Integer, primary_key=True)
+    body = db.column(db.Text, nullable=True)
+    url = db.column(db.Text, nullable=False)
+    short_url = db.column(db.String(3), nullable=False)
+    visits = db.column(db.Integer, default=0)
     created_at = db.column(db.DateTime, default=datetime.now())
     updated_at = db.column(db.DateTime, onupdate=datetime.now())
