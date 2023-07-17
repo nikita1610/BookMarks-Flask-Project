33968480d9088a2971692a0e52380092ae81f8a1
Nikita <jainnikita381@gmail.com>
2023-07-17 14:34:41 +0530

    Adding models and integrating sqlite

--- src/database.py
+++ src/database.py
@@ -1,19 +1,19 @@
 import string
 import random
 from datetime import datetime
-
+from sqlalchemy.orm import backref
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
 
 class User(db.Model):
-    id = db.column(db.Integer, primary_key=True)
-    username = db.column(db.String(80), unique=True, nullable=False)
-    email = db.column(db.String(120), unique=True, nullable=False)
-    password = db.column(db.Text, nullable=False)
-    created_at = db.column(db.DateTime, default=datetime.now())
-    updated_at = db.column(db.DateTime, onupdate=datetime.now())
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.Text, nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.now())
+    updated_at = db.Column(db.DateTime, onupdate=datetime.now())
     bookmarks = db.relationship('Bookmark',backref='user')
 
 
@@ -22,14 +22,14 @@ class User(db.Model):
     
 
 class Bookmark(db.Model):
-    id = db.column(db.Integer, primary_key=True)
-    body = db.column(db.Text, nullable=True)
-    url = db.column(db.Text, nullable=False)
-    short_url = db.column(db.String(3), nullable=False)
-    visits = db.column(db.Integer, default=0)
-    user_id = db.column(db.Integer,db.ForeignKey('user.id'))
-    created_at = db.column(db.DateTime, default=datetime.now())
-    updated_at = db.column(db.DateTime, onupdate=datetime.now())
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.Text, nullable=True)
+    url = db.Column(db.Text, nullable=False)
+    short_url = db.Column(db.String(3), nullable=False)
+    visits = db.Column(db.Integer, default=0)
+    user_id = db.Column(db.Integer,db.ForeignKey('user.id'))
+    created_at = db.Column(db.DateTime, default=datetime.now())
+    updated_at = db.Column(db.DateTime, onupdate=datetime.now())
 
     def __init__(self,**kwargs):
         super.__init__(**kwargs)
