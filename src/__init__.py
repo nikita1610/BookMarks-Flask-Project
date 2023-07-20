6589baa0d984db47d2856cffdc41964ae693d8af
Nikita <jainnikita381@gmail.com>
2023-07-20 14:52:55 +0530

    Getting started with JWT

--- src/__init__.py
+++ src/__init__.py
@@ -1,6 +1,7 @@
 import os
 
 from flask import Flask
+from flask_jwt_extended import JWTManager
 
 from src.auth import auth
 from src.bookmarks import bookmarks
@@ -14,13 +15,17 @@ def create_app(test_config=None):
         app.config.from_mapping(
             SECRET_KEY=os.environ.get("SECRET_KEY"),
             SQLALCHEMY_DATABASE_URI=os.environ.get("SQLALCHEMY_DATABASE_URI"),
-            SQLALCHEMY_TRACK_MODIFICATIONS = False
+            SQLALCHEMY_TRACK_MODIFICATIONS = False,
+            JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY')
         )
     else:
         app.config.from_mapping(test_config)
 
     db.app = app
     db.init_app(app)
+
+    JWTManager(app)
+
     app.register_blueprint(auth)
     app.register_blueprint(bookmarks)
 
