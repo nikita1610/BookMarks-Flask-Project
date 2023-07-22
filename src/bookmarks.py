9a388b395f3aa0dda9bf879012a075ab246b0519
Nikita <jainnikita381@gmail.com>
2023-07-22 17:47:44 +0530

    added bookmarks get api

--- src/bookmarks.py
+++ src/bookmarks.py
@@ -37,3 +37,40 @@ def create_bookmarks():
         'created_at': bookmark.created_at,
         'updated_at': bookmark.updated_at,
     }), HTTP_201_CREATED
+
+
+@bookmarks.get("/")
+@jwt_required()
+def get_bookmarks():
+    page = request.args.get('page', 1, type=int)
+    per_page = request.args.get('per_page', 5, type=int)
+
+    current_user = get_jwt_identity()
+    bookmarks = Bookmark.query.filter_by(
+        user_id=current_user).paginate(page=page, per_page=per_page)
+
+    data = []
+
+    for bookmark in bookmarks.items:
+        data.append({
+            'id': bookmark.id,
+            'url': bookmark.url,
+            'short_url': bookmark.short_url,
+            'visit': bookmark.visits,
+            'body': bookmark.body,
+            'created_at': bookmark.created_at,
+            'updated_at': bookmark.updated_at,
+        })
+
+    meta = {
+        "page": bookmarks.page,
+        'pages': bookmarks.pages,
+        'total_count': bookmarks.total,
+        'prev_page': bookmarks.prev_num,
+        'next_page': bookmarks.next_num,
+        'has_next': bookmarks.has_next,
+        'has_prev': bookmarks.has_prev,
+
+    }
+
+    return jsonify({'data': data, "meta": meta}), HTTP_200_OK
