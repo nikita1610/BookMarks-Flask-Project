e1f3c4045f17a826ec232c4fc203b501a918c1fb
Nikita <jainnikita381@gmail.com>
2023-07-31 14:47:51 +0530

    Swagger integration for short url

--- src/short_url.py
+++ src/short_url.py
@@ -1,3 +1,4 @@
+from flasgger import swag_from
 from flask import Blueprint, redirect
 
 from src.constants.http_status_codes import *
@@ -5,8 +6,8 @@ from src.database import Bookmark, db
 
 short_url = Blueprint("short_url",__name__)
 
-
 @short_url.get('/<short_url>')
+@swag_from('./docs/short_url/short_url.yaml')
 def redirect_to_url(short_url):
     bookmark = Bookmark.query.filter_by(short_url=short_url).first_or_404() 
     if bookmark:
